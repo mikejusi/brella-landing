2a4065d2142af9bb2ca09ca87a62084c13486058
Jusi <megamahmutpower@gmail.com>
2022-08-19 16:51:56 +0300

    create script brings

---
+++ assets/js/filtration.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('#link1').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow1').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info1').removeClass('d-none');
+    });
+    
+    $('#link2').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow2').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info2').removeClass('d-none');
+    });
+
+    $('#link3').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow3').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info3').removeClass('d-none');
+    }); 
+
+    $('#link4').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow4').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info4').removeClass('d-none');
+    });
+
+    $('#link5').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow5').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info5').removeClass('d-none');
+    });
+
+    $('#link6').click(function(event){event.preventDefault();
+    $('.arrow-script').addClass('d-none');
+    $('#arrow6').removeClass('d-none');
+    $('.text__brings').addClass('d-none');
+    $('#info6').removeClass('d-none');
+    });
+
+});
